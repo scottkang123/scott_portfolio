fb07e4a7b602b5f434bbbea789463a7339bd2bd8
scottkang123 <48114611+scottkang123@users.noreply.github.com>
2025-05-17 23:48:08 -0500

    Updates

---
+++ precache-manifest.76398d3713f8c93ce7dcad65af846fdc.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99cf05d32b501aca89d7446a56ac328e",
+    "url": "/scott_portfolio/index.html"
+  },
+  {
+    "revision": "ba203be214e91824898b",
+    "url": "/scott_portfolio/static/css/3.df879eac.chunk.css"
+  },
+  {
+    "revision": "cdb850750231ef04072d",
+    "url": "/scott_portfolio/static/css/4.d7ceb645.chunk.css"
+  },
+  {
+    "revision": "0b3fc2a6f8cb3d79c1d9",
+    "url": "/scott_portfolio/static/css/main.2a90d718.chunk.css"
+  },
+  {
+    "revision": "b418c9ca5961cbd0a7f7",
+    "url": "/scott_portfolio/static/js/2.07f68987.chunk.js"
+  },
+  {
+    "revision": "d4d701abc30fcef5f714584a64df6e82",
+    "url": "/scott_portfolio/static/js/2.07f68987.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba203be214e91824898b",
+    "url": "/scott_portfolio/static/js/3.d408f7ea.chunk.js"
+  },
+  {
+    "revision": "cdb850750231ef04072d",
+    "url": "/scott_portfolio/static/js/4.fbc0b9de.chunk.js"
+  },
+  {
+    "revision": "0b3fc2a6f8cb3d79c1d9",
+    "url": "/scott_portfolio/static/js/main.642f1107.chunk.js"
+  },
+  {
+    "revision": "5a962dfe39168ce5b51f",
+    "url": "/scott_portfolio/static/js/runtime-main.daf90064.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/scott_portfolio/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ff98e31c3bae1cb21f7af3eb843b26df",
+    "url": "/scott_portfolio/static/media/Denave_New_Logo.ff98e31c.jpg"
+  },
+  {
+    "revision": "e75510e9fc70c1ed6efc24505cc084da",
+    "url": "/scott_portfolio/static/media/Flag_of_Malaysia.svg.e75510e9.png"
+  },
+  {
+    "revision": "95ee411e3c209b4075c04acae930eecc",
+    "url": "/scott_portfolio/static/media/Flag_of_South_Korea.95ee411e.svg"
+  },
+  {
+    "revision": "c3abb7f357e6508a25940c0a33337119",
+    "url": "/scott_portfolio/static/media/Flag_of_the_Republic_of_China.c3abb7f3.svg"
+  },
+  {
+    "revision": "99b453cdb7dc00c0632a42851947b78b",
+    "url": "/scott_portfolio/static/media/Flag_of_the_United_States.svg.99b453cd.png"
+  },
+  {
+    "revision": "13db5e2202aa39b783fded70d877b662",
+    "url": "/scott_portfolio/static/media/ML.13db5e22.jpg"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/scott_portfolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "e063390ed37c9c6ee55f4b5a97051c6a",
+    "url": "/scott_portfolio/static/media/National_Yang-Ming_University_logo.svg.e063390e.png"
+  },
+  {
+    "revision": "59d0f6a56d5a83a2aa965b8ef992f5ba",
+    "url": "/scott_portfolio/static/media/UTAustinLogo.59d0f6a5.png"
+  },
+  {
+    "revision": "39bc9bf2fe86f680dc0dd4cf2be181ba",
+    "url": "/scott_portfolio/static/media/VanderbiltUniversityLogo.39bc9bf2.png"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/scott_portfolio/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "d1e8fb803162800c55cbc18b57899262",
+    "url": "/scott_portfolio/static/media/data.d1e8fb80.jpg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/scott_portfolio/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "2a523acbca042f26fbeb23fcdffa1bdc",
+    "url": "/scott_portfolio/static/media/koreanArmyLogo.2a523acb.jpg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/scott_portfolio/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "32a902840cca71090c80890561f4be7f",
+    "url": "/scott_portfolio/static/media/network.32a90284.jpg"
+  },
+  {
+    "revision": "91ccb92e60eb65fe6bce0a9a49386a4c",
+    "url": "/scott_portfolio/static/media/resume.91ccb92e.pdf"
+  },
+  {
+    "revision": "0c66034784183068df1fdb55de5279d5",
+    "url": "/scott_portfolio/static/media/scott_beach.0c660347.png"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/scott_portfolio/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/scott_portfolio/static/media/talksCardBack.13780cdd.svg"
+  },
+  {
+    "revision": "a23aef94419e924800fb3b2f453a74c0",
+    "url": "/scott_portfolio/static/media/vandymarketplace.a23aef94.png"
+  },
+  {
+    "revision": "27079a974701b2f307d68a9a31874895",
+    "url": "/scott_portfolio/static/media/visa-logo-03.27079a97.png"
+  },
+  {
+    "revision": "303827d09871e62d9b7bfd8512834d49",
+    "url": "/scott_portfolio/static/media/wisconsinUniLogo.303827d0.png"
+  }
+]);
